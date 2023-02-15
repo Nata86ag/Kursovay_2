415f42965c6bb411c4c310cc5cec27054fa76f7b
Natalya Kolotygina <natahud86ag@mail.ru>
2023-02-15 23:32:09 +0500

    first commit

---
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -0,0 +1,53 @@
+package ru.netology.javacore;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TodoServer {
+
+    private final int port;
+    private final Todos todos;
+
+    public TodoServer(int port, Todos todos) {
+        this.port = port;
+        this.todos = todos;
+    }
+
+    public void start() throws IOException {
+        System.out.println("Starting server at " + port + "...");
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            while (true) {
+
+                try (Socket socket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                ) {
+                    final String inputJsonTask = in.readLine();
+                    final JsonObject root = JsonParser.parseString(inputJsonTask).getAsJsonObject();
+
+                    String type = root.get("type").toString().replaceAll("\"", "");
+                    String task = root.get("task").toString().replaceAll("\"", "");
+
+                    switch (type) {
+                        case "ADD":
+                            todos.addTask(task);
+                            break;
+                        case "REMOVE":
+                            todos.removeTask(task);
+                            break;
+                    }
+                    out.println("Tasks: " + todos.getAllTasks());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Can't start server");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -0,0 +1,23 @@
+package ru.netology.javacore;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Todos {
+    private final List<String> todos = new ArrayList<>();
+
+    public void addTask(String task) {
+        todos.add(task);
+    }
+
+    public void removeTask(String task) {
+
+        todos.remove(task);
+    }
+
+    public String getAllTasks() {
+        return todos.stream()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.joining(" "));
+    }
+}
